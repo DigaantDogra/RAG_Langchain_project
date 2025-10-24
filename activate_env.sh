5b11fef2fcd06d6ee15fba74d286c4b479df9873
Digaant Singh Dogra <dogra057@gmail.com>
2025-10-23 22:20:10 -0400

    virtual env creation and requirments set

---
+++ activate_env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Script to activate the virtual environment for the RAG LangChain project
+
+echo "Activating virtual environment..."
+source venv/bin/activate
+echo "Virtual environment activated!"
+echo "You can now run your Python scripts with LangChain."
+echo ""
+echo "To deactivate, simply type: deactivate"
