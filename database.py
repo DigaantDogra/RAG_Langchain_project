3ccb3dd24bff12cd3d86b39d8ac331519fd480a7
Digaant Singh Dogra <dogra057@gmail.com>
2025-10-27 21:47:19 -0400

    updated database functions

--- database.py
+++ database.py
@@ -1,18 +1,42 @@
 from langchain_community.document_loaders import DirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_ollama import OllamaEmbeddings
+import os
+import shutil
+from langchain_community.vectorstores import Chroma
 
 DATA_PATH = "Md_Files"
+CHROMA_DB_PATH = "chroma_db"
 
-def load_documents(data_path):
-    loader = DirectoryLoader(data_path, glob="*.md")
+# Generate the vector database
+def genetate_store(documents):
+    documents = load_documents(DATA_PATH)
+    text_splitter = split_documents(documents)
+    save_to_chroma(text_chunks)
+
+def split_text(documents):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200,length_function=len, add_start_index=True)
+    text_chunks = text_splitter.split_documents(documents)
+    print(f"Split {len(documents)} documents into {len(text_chunks)} chunks")
+    return text_chunks
+
+def load_documents():
+    loader = DirectoryLoader(DATA_PATH, glob="*.md")
     documents = loader.load()
     return documents
 
-documents = load_documents(DATA_PATH)
+# Creating a vector database
+def save_to_chroma(text_chunks):
+    print(f"Saving {len(text_chunks)} chunks to Chroma")
 
-# split the documents into chunks
+    if os.path.exists(CHROMA_DB_PATH):
+        shutil.rmtree(CHROMA_DB_PATH)
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200,length_function=len, add_start_index=True)
-text_chunks = text_splitter.split_documents(documents)
+    # Using Ollama nomic-embed-text model
+    embeddings = OllamaEmbeddings(model="nomic-embed-text")
+    vectorstore = Chroma.from_documents(text_chunks, embedding=embeddings, persist_directory=CHROMA_DB_PATH)
+    vectorstore.persist()
 
-print(f"Split {len(documents)} documents into {len(text_chunks)} chunks")
+    print(f"saved {len(text_chunks)} chunks to {CHROMA_DB_PATH}")
+# Call the function to create the vector database
+save_to_chroma(text_chunks)
